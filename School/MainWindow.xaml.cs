e3bc187ac1b674c2f9faed8afe41c4bf150caef7
jywkim <jkim589@gmail.com>
2018-10-30 00:41:15 -0400

    remove student and enable user to save

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -111,7 +111,12 @@ namespace School
                     "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question,
                     MessageBoxResult.No);
                     // TODO: Exercise 3: Task 3a: If the user clicked Yes, remove the student from the database
-                    // TODO: Exercise 3: Task 3b: Enable saving (changes are not made permanent until they are written back to the database)
+                    if (response == MessageBoxResult.Yes)
+                    {
+                        this.schoolContext.Students.DeleteObject(student);
+                        // TODO: Exercise 3: Task 3b: Enable saving (changes are not made permanent until they are written back to the database)
+                        saveChanges.IsEnabled = true;
+                    }
                     break;
             }
         }
