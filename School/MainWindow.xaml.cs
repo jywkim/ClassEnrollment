2ccdeb08d49a4aa501b1c59e7aca0554d6eb600e
jywkim <jkim589@gmail.com>
2018-10-30 00:37:53 -0400

    prompt user to confirm deletion

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -106,6 +106,10 @@ namespace School
                 // TODO: Exercise 3: Task 1a: If the user pressed Delete, remove the currently selected student
                 case Key.Delete: student = this.studentsList.SelectedItem as Student;
                     // TODO: Exercise 3: Task 2a: Prompt the user to confirm that the student should be removed
+                    MessageBoxResult response = MessageBox.Show(
+                    string.Format("Remove {0}", student.FirstName + " " + student.LastName),
+                    "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question,
+                    MessageBoxResult.No);
                     // TODO: Exercise 3: Task 3a: If the user clicked Yes, remove the student from the database
                     // TODO: Exercise 3: Task 3b: Enable saving (changes are not made permanent until they are written back to the database)
                     break;
