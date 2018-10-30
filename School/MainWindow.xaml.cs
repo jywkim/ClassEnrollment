66d36a94dea6b7a94a65d202c9b86311e127d3d3
jywkim <jkim589@gmail.com>
2018-10-29 22:46:39 -0400

    initialize StudentForm window and populate w/ details

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -62,7 +62,12 @@ namespace School
             // TODO: Exercise 1: Task 1a: If the user pressed Enter, edit the details for the currently selected student
                 case Key.Enter: Student student = this.studentsList.SelectedItem as Student;
             // TODO: Exercise 1: Task 2a: Use the StudentsForm to display and edit the details of the student
+                StudentForm sf = new StudentForm();
             // TODO: Exercise 1: Task 2b: Set the title of the form and populate the fields on the form with the details of the student
+                sf.Title = "Edit Student Details";
+                sf.firstName.Text = student.FirstName;
+                sf.lastName.Text = student.LastName;
+                sf.dateOfBirth.Text = student.DateOfBirth.ToString("d");
             // TODO: Exercise 1: Task 3a: Display the form
             // TODO: Exercise 1: Task 3b: When the user closes the form, copy the details back to the student
             // TODO: Exercise 1: Task 3c: Enable saving (changes are not made permanent until they are written back to the database)
