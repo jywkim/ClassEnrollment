95d09108963e12dcecf8dddb54c35b06c4d9a3e4
jywkim <jkim589@gmail.com>
2018-10-30 20:14:04 -0400

    add concurrency/update/general exception handling

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -173,14 +173,36 @@ namespace School
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
             // TODO: Exercise 3: Task 2b: Save the changes by calling the SaveChanges method of the schoolContext object
-            // Save the changes
-            this.schoolContext.SaveChanges();
+            try
+            {
+                // Save the changes
+                this.schoolContext.SaveChanges();
 
-            // Disable the Save button (it will be enabled if the user makes more changes)
-            saveChanges.IsEnabled = false;
+                // Disable the Save button (it will be enabled if the user makes more changes)
+                saveChanges.IsEnabled = false;
+            }
             // TODO: Exercise 3: Task 3a: If an OptimisticConcurrencyException occurs then another user has changed the same students earlier, then overwrite their changes with the new data (see the lab instructions for details)
+            catch (OptimisticConcurrencyException)
+            {
+                // If the user has changed the same students earlier, then overwrite their changes with the new data
+                this.schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+                this.schoolContext.SaveChanges();
+            }
             // TODO: Exercise 3: Task 3b: If an UpdateException occurs then report the error to the user and rollback (see the lab instructions for details)
+            catch (UpdateException uEx)
+            {
+                // If some sort of database exception has occurred, then display the reason for the exception and rollback
+
+                MessageBox.Show(uEx.InnerException.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+            }
             // TODO: Exercise 3: Task 3c: If some other sort of error has occurs, report the error to the user and retain the data so the user can try again - the error may be transitory (see the lab instructions for details)
+            catch (Exception ex)
+            {
+                // If some other exception occurs, report it to the user
+                MessageBox.Show(ex.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+            }
         }
     }
 
