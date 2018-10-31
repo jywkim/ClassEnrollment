900d85bacfe1ef9ad6e87e49727c033313075c22
jywkim <jkim589@gmail.com>
2018-10-30 20:09:25 -0400

    add code to save changes back to db

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -6,7 +6,9 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using School.Data;
-
+using System.Data;
+using System.Data.Objects;
+// TODO: Exercise 3: Task 2a: Bring the System.Data and System.Data.Objects namespaces into scope
 
 namespace School
 {
@@ -170,7 +172,15 @@ namespace School
         // Save changes back to the database and make them permanent
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
+            // TODO: Exercise 3: Task 2b: Save the changes by calling the SaveChanges method of the schoolContext object
+            // Save the changes
+            this.schoolContext.SaveChanges();
 
+            // Disable the Save button (it will be enabled if the user makes more changes)
+            saveChanges.IsEnabled = false;
+            // TODO: Exercise 3: Task 3a: If an OptimisticConcurrencyException occurs then another user has changed the same students earlier, then overwrite their changes with the new data (see the lab instructions for details)
+            // TODO: Exercise 3: Task 3b: If an UpdateException occurs then report the error to the user and rollback (see the lab instructions for details)
+            // TODO: Exercise 3: Task 3c: If some other sort of error has occurs, report the error to the user and retain the data so the user can try again - the error may be transitory (see the lab instructions for details)
         }
     }
 
